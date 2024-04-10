6388007cc3eb31a12384e2d36dfb529cb74e8976
petterSantos <newpetter@gmail.com>
2024-04-10 16:18:42 -0500

    correccion Databases

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,15 +83,14 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+
        # 'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(
+      'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
         )
-    }
 }
 
 
